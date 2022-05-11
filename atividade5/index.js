f807342281ddb552c88af8a6b8376125c3ab7379
eltonk2k <elieltonpb1@gmail.com>
2022-05-11 19:39:59 -0300

    exercicio

--- atividade5/index.js
+++ atividade5/index.js
@@ -1,5 +1,5 @@
 
-async function callAsync() {
+async function getEstados() {
     try {
         const res = await
             fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados");
@@ -11,6 +11,8 @@ async function callAsync() {
         resultados.forEach(element => {
             const varrerListEstados = document.createElement("option") 
             varrerListEstados.innerText = element.nome
+            varrerListEstados.value = element.sigla
+            
             estados.appendChild(varrerListEstados)
         });
         
@@ -18,25 +20,22 @@ async function callAsync() {
         console.log(error);
     }
 }
-window.addEventListener("load", callAsync)
+window.addEventListener("load", getEstados)
 
 
-
-
-
-async function callAsync() {
+async function getMunicipios() {
     try {
         const res = await
             fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados/{UF}/municipios");
         const resultados = await res.json();
         console.log(resultados)
 
-        const estados = document.getElementById("estado")
+        const estados = document.getElementById("UF")
 
         resultados.forEach(element => {
-            const varrerListEstados = document.createElement("option") 
-            varrerListEstados.innerText = element.nome
-            estados.appendChild(varrerListEstados)
+            const varrerListMunicipos = document.createElement("option") 
+            varrerListMunicipos.value = element.UF
+            UF.appendChild(varrerListMunicipos)
         });
         
     } catch (error) {
