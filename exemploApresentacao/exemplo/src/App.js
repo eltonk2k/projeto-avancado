d460439ecda5043028dd4a4dd54fa6bfdf49d39f
eltonk2k <elieltonpb1@gmail.com>
2022-05-31 18:02:26 -0300

    apresentacao

---
+++ exemploApresentacao/exemplo/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState, useLayoutEffect, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./index.css"
+
+export default function App() {
+
+  const BlinkyRender = () => {
+    const [value, setValue] = useState(0);
+
+    useLayoutEffect(() => {
+      if (value === 0) {
+        setValue(10 + Math.random() * 200);
+      }
+    }, [value]);
+
+    console.log("render", value);
+
+    return (
+      <div onClick={() => setValue(0)}>Valor: {value}</div>
+    );
+  };
+
+  ReactDOM.render(
+    <BlinkyRender />,
+    document.querySelector("#root")
+  );
+
+}
+
